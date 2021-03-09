5a3789f8238ac53bfae6e3bfee2b110e962e94a9
Danilo C. Graciano <danilocgraciano@gmail.com>
2021-03-09 20:57:56 -0300

    Commit Inicial - Fim dia 2

---
+++ reactjs/01-github-explorer/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isdevelopment = process.env.NODE_ENV != 'production';
+
+module.exports = {
+    mode: isdevelopment ? 'development' : 'production',
+    devtool: isdevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        hot: true,
+    },
+    plugins: [
+        isdevelopment && new ReactRefreshWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ].filter(Boolean),
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isdevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    }
+}
