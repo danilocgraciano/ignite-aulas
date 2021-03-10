70398cfcc6c305ec68227969e39f19d9e40fe6c3
Danilo C. Graciano <danilocgraciano@gmail.com>
2021-03-10 20:04:17 -0300

    Fetch on github api.

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,21 +1,28 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from '../components/RepositoryItem';
 
 import '../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    // será executada uma única vez, assim que o componente for renderizado em tela
+    useEffect(() => {
+        fetch('https://api.github.com/users/danilocgraciano/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data));
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(
+                    repository => {
+                        return <RepositoryItem key={repository.id} repository={repository} />;
+                    }
+                )}
             </ul>
         </section>
     );
