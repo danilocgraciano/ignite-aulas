71274d550f1083319982b512666b10299ce81336
Danilo C. Graciano <danilocgraciano@gmail.com>
2021-03-25 18:26:11 -0300

    novas implementações.

---
+++ ignews/src/components/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+        <button
+            type="button"
+            className={styles.button}
+        >
+            <FaGithub color="#04d361" />
+            Danilo C Graciano
+            <FiX color="#737380" className={styles.closeIcon}/>
+        </button>
+    ) : (
+        <button
+            type="button"
+            className={styles.button}
+        >
+            <FaGithub color="#eba417" />
+            Sign in With Github
+        </button>
+    )
+
+}
