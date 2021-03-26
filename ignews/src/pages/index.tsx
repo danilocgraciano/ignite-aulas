6982d83b2c16d148f2b582f7b7d109794344e906
Danilo C. Graciano <danilocgraciano@gmail.com>
2021-03-26 18:22:01 -0300

    novas features

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,22 @@
+import { /*GetServerSideProps,*/ GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+// Client-side
+// Server-side
+// Static Site Generation
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    ammount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,9 +29,9 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.ammount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
 
@@ -27,4 +40,25 @@ export default function Home() {
 
     </>
   )
+}
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1IYZvX2eZvKYlo2CmAfrP3mv');
+
+  const product = {
+    priceId: price.id,
+    ammount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),//preco em centavos
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
+  }
 }
