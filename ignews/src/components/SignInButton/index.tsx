854742a1664a68d28057dbac4046fd4284843c03
Danilo C. Graciano <danilocgraciano@gmail.com>
2021-03-27 09:36:54 -0300

    next-auth github implementado

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,25 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
 
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button
             type="button"
             className={styles.button}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            Danilo C Graciano
-            <FiX color="#737380" className={styles.closeIcon}/>
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button
             type="button"
             className={styles.button}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in With Github
