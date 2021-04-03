5b220489a085e31c00d753d766af17c17d96bd4d
Danilo C. Graciano <danilocgraciano@gmail.com>
2021-04-03 09:08:07 -0300

    novas implementações

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss';
 import { useSession, signIn } from 'next-auth/client';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
     priceId: string;
@@ -10,11 +11,17 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
     const [session] = useSession();
+    const router = useRouter();
 
     async function handleSubscribe() {
         if (!session) {
             signIn('github')
             return;
+        }
+
+        // @ts-ignore
+        if (session.activeSubscription) {
+            return router.push('/posts');
         }
 
         try {
@@ -33,6 +40,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
         <button
             type="button"
             className={styles.button}
+            onClick={handleSubscribe}
         >
             Subscribe Now
         </button>
