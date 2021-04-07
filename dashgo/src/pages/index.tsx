3b6133b0a364939f033e3f2708aad8d78c470969
Danilo C. Graciano <danilocgraciano@gmail.com>
2021-04-07 20:31:18 -0300

    Criado componente login

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,8 +1,41 @@
 
-export default function Home() {
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from '../components/Form/Input'
+
+export default function SignIn() {
   return (
-    <>
-      <h1>DashGO</h1>
-    </>
+    // vw = viewport width
+    // vh = viewport height
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="360px"
+        bgColor="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          
+          <Input type="email" name="email" label="E-mail"></Input>
+          <Input type="password" name="password" label="Senha"></Input>
+
+          <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+          >Entrar</Button>
+        </Stack>
+      </Flex>
+
+    </Flex>
   )
 }
