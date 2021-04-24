27384231163374cdcaa12132bc2373f6873fb5fc
Danilo C. Graciano <danilocgraciano@gmail.com>
2021-04-24 10:31:09 -0300

    noas implementações

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,11 +1,15 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, Spinner, useBreakpointValue } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import Link from 'next/link';
+
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
-import Link from 'next/link';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
+
+    const { data, isLoading, isFetching, error } = useUsers();
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -21,7 +25,10 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
                         <Link href="/users/create">
                             <Button
@@ -36,84 +43,59 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Danilo C. Graciano</Text>
-                                        <Text fontSize="sm" color="gray.300">danilocgraciano@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Danilo C. Graciano</Text>
-                                        <Text fontSize="sm" color="gray.300">danilocgraciano@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Danilo C. Graciano</Text>
-                                        <Text fontSize="sm" color="gray.300">danilocgraciano@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao carregar os dados</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
+                                        <Th width="8"></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
+                                                <Td>
+                                                    <Button
+                                                        as="a"
+                                                        size="sm"
+                                                        fontSize="sm"
+                                                        colorScheme="purple"
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                                    >
+                                                        Editar
+                                                    </Button>
+                                                </Td>
+                                            </Tr>
+                                        )
+                                    })}
+
+                                </Tbody>
+                            </Table>
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
---
+++ dashgo/src/services/mirage/index.ts
@@ -0,0 +1,63 @@
+import { createServer, Model, Factory, Response } from 'miragejs';
+
+type User = {
+    name: string;
+    email: string;
+    created_at: string;
+};
+
+export function makeServer() {
+    const server = createServer({
+
+        models: {
+            user: Model.extend<Partial<User>>({})
+        },
+
+        factories: {
+            user: Factory.extend({
+                name(index: number) {
+                    return `User ${index + 1}`;
+                },
+                email(index: number) {
+                    return `user${index + 1}@email.com`;
+                },
+                createdAt() {
+                    return new Date();
+                }
+            })
+        },
+
+        seeds(server) {
+            server.createList('user', 200);
+        },
+
+        routes() {
+            this.namespace = 'api';
+            this.timing = 750;
+
+            this.get('/users', function (schema, request) {
+                const { page = 1, per_page = 10 } = request.queryParams;
+
+                const total = schema.all('user').length;
+
+                const pageStart = (Number(page) - 1) * Number(per_page);
+                const pageEnd = pageStart + Number(per_page);
+
+                const users = this.serialize(schema.all('user')).
+                    users.slice(pageStart, pageEnd);
+
+                return new Response(
+                    200,
+                    { 'x-total-count': String(total) },
+                    { users }
+                );
+            });
+            this.post('/users');
+
+            this.namespace = '';
+            this.passthrough();
+        }
+    });
+
+    return server;
+}
